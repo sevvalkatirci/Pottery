8056fe5aec4c43006a76cdb9c842446fd79ba902
sevvalkatirci <sevvalkatirci99@gmail.com>
2021-04-13 14:41:07 +0300

    First Commit

---
+++ Assets/Scripts/KnifeMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnifeMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float movementSpeed;
+    [SerializeField]
+    private float hitDamage;
+    [SerializeField]
+    private WoodRotate woodRotate;
+    private Rigidbody knifeBody;
+    private Vector3 movementVector;
+    private bool isMoving = false;
+
+    private void Start()
+    {
+        knifeBody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        isMoving = Input.GetMouseButton(0);
+        if (isMoving)
+        {
+            movementVector = new Vector3(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"), 0f) * movementSpeed * Time.deltaTime;
+        }
+    }
+    private void FixedUpdate()
+    {
+        if (isMoving)
+        {
+            knifeBody.position += movementVector;
+        }
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        ColliderScale colliderScale = collision.collider.GetComponent<ColliderScale>();
+        if (colliderScale != null)
+        {
+            colliderScale.HitCollider(hitDamage);
+            woodRotate.Hit(colliderScale.index, hitDamage);
+        }
+    }
+}
---
+++ Assets/Scripts/WoodRotate.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class WoodRotate : MonoBehaviour
+{
+    [SerializeField]
+    private Transform woodTransform;
+    [SerializeField]
+    private float rotationDuration;
+    [SerializeField]
+    private Vector3 rotationVector;
+    [SerializeField]
+    private SkinnedMeshRenderer skinnedMeshRenderer;
+
+    private void Start()
+    {
+        woodTransform
+            .DOLocalRotate(rotationVector, rotationDuration, RotateMode.FastBeyond360)
+            .SetLoops(-1, LoopType.Restart)
+            .SetEase(Ease.Linear);
+    }
+
+    public void Hit(int keyIndex,float damage)
+    {
+       
+        float colliderHeight = 2.398268f;
+        float newWeight = skinnedMeshRenderer.GetBlendShapeWeight(keyIndex) + damage * (100f / colliderHeight);
+        skinnedMeshRenderer.SetBlendShapeWeight(keyIndex, newWeight);
+
+    }
+
+}
